d192b3c831c69846d8cd812d00fef976042e55b4
Anderson Carlos Sales de Jesus <andersoncarlos01@hotmail.com>
2022-12-26 17:50:00 -0300

    validar email de cadastro

--- cria_usuario_comum.php
+++ cria_usuario_comum.php
@@ -1,7 +1,6 @@
 <?php
     function empt(){return;}
     function cadastro(){
-
         //criando texto/dados do usuario
         $texto = PHP_EOL . 5 . '#' .  $_POST['emailCadastro'] . '#' . $_POST['senhaCadastro'] . '#' . 2 ;
         //abrindo arquivo
@@ -10,24 +9,29 @@
         fwrite($arquivo, $texto);
         //fechando arquivo
         fclose($arquivo);
-        
+    }
+    $cadastro = false;
+    if($_POST['emailCadastro'] == '' || $_POST['senhaCadastro'] == ''){
+        header('Location: cria_usuario.php?cod=campovazio');
+    } else{
+        //verificar se email ja foi cadastrado
+        $arquivo = fopen('data/banco.ban', 'r');
+        $banco_dados = array();
+
+        while(!feof($arquivo)){//testa o fim de um arquivo end of file//retorna true se chegar ao final do arquivo
+            $registro = fgets($arquivo);//recuperar tudo na linha ou ate a quebra de linha especificada
+            $dado = explode('#', $registro);
+            if($_POST['emailCadastro'] == $dado[1]){
+                header('Location: cria_usuario.php?cod=emailUser');
+            }else{
+                $cadastro = true;
+            }
+        }
     }
 
-    $_POST['emailCadastro'] == '' || $_POST['senhaCadastro'] == '' ? header('Location: cria_usuario.php?cod=campovazio') :  empt();
-    
-    //verificar se email ja foi cadastrado
-    $arquivo = fopen('data/banco.ban', 'r');
-    $banco_dados = array();
-
-    while(!feof($arquivo)){//testa o fim de um arquivo end of file//retorna true se chegar ao final do arquivo
-        $registro = fgets($arquivo);//recuperar tudo na linha ou ate a quebra de linha especificada
-        $dado = explode('#', $registro);
-
-        $dado[1] == $_POST['emailCadastro'] ? header('Location: cria_usuario.php?cod=emailUser'): empt();
+    if($cadastro){
+        cadastro();
+        header('Location: cria_usuario.php?cod=success');
     }
 
-
-    cadastro();
-    //header('Location: cria_usuario.php?cod=success');
-    
 ?>
